3429471fc806fd2c0e919271ac4ddf09e7355b56
Robin Bron <robin@finwo.nl>
2017-10-16 11:15:56 +0200

    Added update script

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Make sure we're in the correct folder
+cd $(dirname $0)
+
+# Update all references
+git fetch --al &>/dev/null
+
+# Fetch hashes
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+# Check what to do
+if [ $LOCAL = $REMOTE ]; then
+  # up-to-date
+  echo -en ""
+elif [ $LOCAL = $BASE ]; then
+  # Updates available
+  git pull
+elif [ $REMOTE = $BASE ]; then
+  # Remote outdated
+  git push
+else
+  echo "Diverged!"
+  exit 1
+fi
