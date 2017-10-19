a46ac34a0b2cd3033b45f79e15ab94170eb578f1
hendrik-vpscash <hendrik@vpscash.nl>
2017-10-19 13:40:06 +0200

    dot support for optional parameters

--- config/regexRepository.js
+++ config/regexRepository.js
@@ -1,6 +1,6 @@
 module.exports.regexRepository = {
   "hbsFile"       : /(.*)\.hbs$/i,
   "%"             : /%/g,
-  "optionalParams": /^\[[\w\- ]+]$/g,
+  "optionalParams": /^\[[\w\- .]+]$/g,
   "protocol"      : /^(https?|s?ftp):\/\//i
 };
